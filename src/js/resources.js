0a3d46e8b2f50ddf534b7980cffe638be2ae699e
Lars-van-Tilburg <62542010+Lars-van-Tilburg@users.noreply.github.com>
2024-06-25 11:04:14 +0200

    werkende

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,14 +2,14 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import { StartScreen } from './startscreen'
 
 const Resources = {
-    Player: new ImageSource('./images/speler.png'),
-    Land: new ImageSource('./images/wall.png'),
+    Player: new ImageSource('images/speler.png'),
+    Land: new ImageSource('images/wall.png'),
     Gun: new ImageSource('images/gun.png'),
     Platform: new ImageSource('images/BrownOnLarge.png'),
     Jump: new ImageSource('images/spring.png'),
     Coin: new ImageSource('images/kaas.png'),
-    Stone: new ImageSource('./images/mes.png'),
-    StartScreen: new Image('./images/wall.png'),
+    Stone: new ImageSource('images/mes.png'),
+    StartScreen: new Image('images/wall.png'),
 }
 const ResourceLoader = new Loader([
     Resources.Player,
